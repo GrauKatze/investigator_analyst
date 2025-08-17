07d373419add3720b52f8ed9e513d02d174c3e65
GrauKatze <GrauKatze@protonmail.com>
2025-08-17 20:37:35 +0300

    add base function

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,28 @@
 #![warn(missing_docs)]
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! Analyse metadata files
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// modile for start analys
+pub mod file_identification {
+    use std::path::Path;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// to know file
+    pub fn _file_ident(file_path: String) -> Result<(), String> {
+        if Path::exists(Path::new(&file_path)) {
+            _take_file_metadata(&file_path).unwrap();
+            _check_file_executble(&file_path).unwrap();
+            Ok(())
+        } else {
+            Err(format!("path is not valid, path: \"{}\"", file_path))
+        }
+    }
+
+    fn _take_file_metadata(_file_path: &String) -> Result<(), String> {
+        Ok(())
+    }
+
+    ///Return executble format
+    fn _check_file_executble(_file_path: &String) -> Result<String, String> {
+        Ok("x86".to_string())
     }
 }
