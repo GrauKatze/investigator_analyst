c4a3f4cebd917caf51f652fe9228b2ee60910072
GrauKatze <GrauKatze@protonmail.com>
2025-11-19 00:59:56 +0300

    добавил функциональности для проверик работы

--- src/lib.rs
+++ src/lib.rs
@@ -4,21 +4,20 @@
 
 /// modile for start analys
 pub mod file_identification {
-    use std::path::Path;
+    use std::{fs::Metadata, path::Path};
 
     /// to know file
-    pub fn _file_ident(file_path: String) -> Result<(), String> {
+    pub fn _file_ident(file_path: String) -> Result<Metadata, String> {
+        println!("{}", &file_path);
         if Path::exists(Path::new(&file_path)) {
-            _take_file_metadata(&file_path).unwrap();
-            _check_file_executble(&file_path).unwrap();
-            Ok(())
+            Ok(_take_file_metadata(&file_path).unwrap())
         } else {
             Err(format!("path is not valid, path: \"{}\"", file_path))
         }
     }
 
-    fn _take_file_metadata(_file_path: &String) -> Result<(), String> {
-        Ok(())
+    fn _take_file_metadata(_file_path: &String) -> Result<Metadata, String> {
+        Ok(Path::new(&_file_path).metadata().unwrap())
     }
 
     ///Return executble format
