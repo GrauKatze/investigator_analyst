dcad714ccb24d27f09dbb548c76370a16b384a93
GrauKatze <GrauKatze@protonmail.com>
2026-03-05 10:27:38 +0300

    refactor code

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -2,12 +2,5 @@ fn main() {
     println!(env!("CARGO_PKG_NAME"));
     println!("===START===");
 
-    match match analyst::configuration::init(Some(std::env::args().skip(1).collect())) {
-        Ok(cnf) => analyst::configuration::run(cnf),
-        Err(msg) => Err(msg),
-    } {
-        Ok(()) => (),
-        Err(msg) => println!("{}", msg),
-    }
     println!("===END===");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,61 @@
 //! Analyse metadata files
 
-use std::{fs::File, path::Path};
+enum Config {
+    Version,
+    Help,
+    Command(Option<Vec<String>>),
+}
 
-pub mod configuration;
+impl Config {
+    fn build(init_options: &Option<Vec<String>>) -> Result<Config, String> {
+        match init_options {
+            None => Ok(Config::Help),
+            Some(options) => match options.get(0) {
+                Some(p) => match p.as_str() {
+                    "-h" | "--help" | "h" | "help" => Ok(Config::Help),
+                    "-v" | "--version" | "v" | "version" => Ok(Config::Version),
+                    _ => Err(format!(
+                        "not found this arguments\n{{ {:?} }}",
+                        options.iter().collect::<Vec<&String>>()
+                    )),
+                },
+                None => Err("Need args".to_string()),
+            },
+        }
+    }
+}
+pub struct Analyser {
+    configuration: Config,
+}
 
-mod fl_analyse;
+impl Analyser {
+    pub fn build(init_options: &Option<Vec<String>>) -> Result<Analyser, String> {
+        let conf = Config::build(init_options);
+        match conf {
+            Ok(conf) => Ok(Analyser {
+                configuration: conf,
+            }),
+            Err(msg) => Err(msg),
+        }
+    }
+    pub fn run(&self) -> Result<(), String> {
+        match &self.configuration {
+            Config::Help => Self::write_help_msg(),
+            Config::Version => Self::write_version_msg(),
+            _ => Err("Error aanlis config".to_string()),
+        }
+    }
 
-/// Проверка на существование файла
-fn is_file_exists(fl: &Path) -> Result<File, String> {
-    match fl.exists() {
-        true => Ok(File::open(fl).unwrap()),
-        false => Err("Not correct path".to_string()),
+    pub fn write_help_msg() -> Result<(), String> {
+        println!(env!("CARGO_PKG_NAME"));
+        println!(env!("CARGO_PKG_VERSION"));
+        Ok(())
+    }
+    pub fn write_version_msg() -> Result<(), String> {
+        println!(env!("CARGO_PKG_VERSION"));
+        Ok(())
     }
 }
 
 #[cfg(test)]
-mod tests {
-}
+mod tests {}
